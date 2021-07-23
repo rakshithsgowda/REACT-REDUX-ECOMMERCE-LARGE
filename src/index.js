db2efcbb5361932b7f12e96501a94135e2ed45e0
rakshith <rakshit.s.gowda@gmail.com>
2021-07-23 13:32:35 +0530

    register form

--- src/index.js
+++ src/index.js
@@ -2,8 +2,8 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import { BrowserRouter } from 'react-router-dom'
 import App from './App'
-import 'antd/dist/antd.css'
 import 'mdb-react-ui-kit/dist/css/mdb.min.css'
+import 'antd/dist/antd.css'
 import './index.css'
 
 ReactDOM.render(
